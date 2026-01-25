97a3c12cd559927bed91b36c870c9e3dc2d7dfba
darrenthebozz <darrenthebozz1@gmail.com>
2026-01-25 16:47:44 +0000

    Add German option

--- website/src/modules/GGEUsersTable.js
+++ website/src/modules/GGEUsersTable.js
@@ -89,7 +89,7 @@ export default function GGEUserTable(props) {
 
         return <TableContainer component={Paper}>
             <Box sx={{ display: 'flex', justifyContent: 'flex-end', p: 1 }}>
-                <Button size="small" variant={language === 'tr' ? 'contained' : 'outlined'} onClick={() => setLanguage('tr')} sx={{ mr: 1 }}>TR</Button>
+                <Button size="small" variant={language === 'tr' ? 'contained' : 'outlined'} onClick={() => setLanguage('de')} sx={{ mr: 1 }}>DE</Button>
                 <Button size="small" variant={language === 'tr' ? 'contained' : 'outlined'} onClick={() => setLanguage('tr')} sx={{ mr: 1 }}>TR</Button>
                 <Button size="small" variant={language === 'en' ? 'contained' : 'outlined'} onClick={() => setLanguage('en')}>EN</Button>
             </Box>
